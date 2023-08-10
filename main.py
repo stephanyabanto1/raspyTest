fc0a4319afc5fadcb1984cccce341cb8c2a3b4e9
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-10 17:18:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ import socketio
 sio = socketio.Client()
 
 gpio.setmode(gpio.BCM)
+
+#ENA
 gpio.setup(27, gpio.OUT)
 gpio.setup(17, gpio.OUT)
 gpio.setup(4, gpio.OUT)
+#End of ENA 
+
+#ENB
+gpio.setup(5, gpio.OUT)
+gpio.setup(6, gpio.OUT)
+gpio.setup(13, gpio.OUT)
+#end of ENB
+
+
 
 def forward():
     gpio.output(17,True )
@@ -20,9 +31,24 @@ def backward():
 
 def turnOn():
     gpio.output(27,True)
+    gpio.output(5, True)
 
 def turnOff():
     gpio.output(27, False)
+    gpio.output(5, False)
+
+def turnRight():
+    gpio.output(6, True)
+    gpio.output(13, False)
+    gpio.output(17, False)
+    gpio.output(4, True)
+
+def turnLeft(){
+    gpio.output(6, False)
+    gpio.output(13, True)
+    gpio.output(17, True)
+    gpio.output(4, False)
+}
 
 @sio.event
 def connect():
@@ -47,6 +73,16 @@ def piTurnedOff():
 def piTurnedOn():
     print("Turned on")
     turnOn()
+
+@sio.event
+def piTurnedRight():
+    print("Turned Right")
+    turnRight()
+
+@sio.event
+def piTurnedLeft():
+    print("Turned left")
+    turnLeft()
 
 @sio.event
 def disconnect():
